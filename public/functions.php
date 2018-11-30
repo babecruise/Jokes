977826bbc444ac51b32f9ed0510291a173e23fd1
babecrusie <fiduh987@gmail.com>
2018-11-30 10:07:58 +0200

    'commit'

--- public/functions.php
+++ public/functions.php
@@ -5,7 +5,7 @@
             $ch = curl_init();
 
             // set url 
-            curl_setopt($ch, CURLOPT_URL, "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"); 
+            curl_setopt($ch, CURLOPT_URL, "https://safe-falls-22549.herokuapp.com/random_joke"); 
     
             //return the transfer as a string 
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
